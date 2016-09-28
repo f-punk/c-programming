76bf2f89c3a024d01799cdf69353410a3f35e1af
f-punk <mjchris2968@googlemail.com>
2016-09-28 09:59:10 +0100

    Change continue to null statement

--- ch06/10.c
+++ ch06/10.c
@@ -21,7 +21,7 @@ int main(void)
         sum = year * 10000 + month * 100 + day;
         
         if (sum == 0) {
-            continue;
+            ;
         }
         else if (sum < earliest) {
             earliest = sum;
